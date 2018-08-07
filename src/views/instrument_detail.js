072a4c5d0d30e958730bfcfb6eeadcec7045756d
Iona Macbeth <ionalmacbeth@gmail.com>
2018-08-07 18:03:41 +0100

    list of examples rendering

--- src/views/instrument_detail.js
+++ src/views/instrument_detail.js
@@ -15,11 +15,23 @@ InstrumentDetail.prototype.bindEvents = function () {
 InstrumentDetail.prototype.render = function(family){
   const header = document.createElement('h1')
   const infoParagraph = document.createElement('p');
+  const list = document.createElement('ul');
+
   header.textContent = `${family.name}`
   infoParagraph.textContent = `${family.description}`;
+  list.textContent = 'Examples of this family:'
+  for(i = 0; i < family.instruments.length; i++){
+    const listItem = document.createElement('li');
+    console.log(family.instruments[i]);
+    listItem.textContent = family.instruments[i];
+    list.appendChild(listItem);
+  }
+
   this.container.innerHTML = '';
   this.container.appendChild(header);
   this.container.appendChild(infoParagraph);
+  this.container.appendChild(list)
+
 };
 
 module.exports = InstrumentDetail;
