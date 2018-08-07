045e059d53a67f5844aaa60a22413c0ee5f5a910
Iona Macbeth <ionalmacbeth@gmail.com>
2018-08-07 16:30:49 +0100

    populate dropdown

--- src/views/select_view.js
+++ src/views/select_view.js
@@ -4,5 +4,24 @@ const SelectView = function (element) {
   this.element = element;
 };
 
+SelectView.prototype.bindEvents = function () {
+  PubSub.subscribe('InstrumentFamilies:all-instruments-ready', (event) => {
+    const allInstruments = event.detail;
+    this.populate(allInstruments);
+  })
+  this.element.addEventListener('change', (event) => {
+  const selectedIndex = event.target.value;
+  PubSub.publish('SelectView:change', selectedIndex);
+  });
+};
+
+SelectView.prototype.populate = function(instrumentsData){
+  animalsData.forEach((instrument, index) => {
+    const option = document.createElement('option');
+    option.textContent = instrument.name;
+    option.value = index;
+    this.element.appendChild(option);
+  })
+}
 
 module.exports = SelectView;
